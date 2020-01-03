9db8383c208e5c91001c85c1af846652045949ef
tombiskupski <tom.biskupski@aim.com>
2020-01-03 15:56:11 +0100

    Datenbank hinzugefügt

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const port = process.env.PORT || 3000;
 const app = express();
+const db = new sqlite3.Database('./datenbank/data.db');
 
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res) {
- 
-    res.render('pages/index', {
-      
-    });
+    
+  var pageTitle = "Versuch Nummero 1"
+db.all('SELECT * FROM test', (err, rows) => {
+  console.log("get / index funktion")
+  res.render('pages/index', {
+    title: pageTitle,
+    data: rows
   });
+});
+});
 
 app.listen(port, function() {
     console.log(`Server listening on port ${port}…`)

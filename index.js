d6e463557b555b39b60097084374fdd7d3e57aba
Trung Son Nguyen <91ngtr1bif@hft-stuttgart.de>
2020-01-09 00:44:37 +0100

    test

--- index.js
+++ index.js
@@ -10,39 +10,41 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
-  res.render('pages/index');
+  res.render('pages/');
 });
 
-app.post('/index', (req, res, next) => {
-  let sql = `SELECT * FROM User WHERE username = "${req.body.username}" AND password = "${req.body.password}"`;
+app.post('/', (req, res, next) => {
+  let sql = 'SELECT * FROM user WHERE username = "${req.body.username}" AND password = "${req.body.password}"';
   var x;
- 
+  console.log('db test');
   db.all(sql, (err, rows) => {
+    console.log('db');
    if (err) {
+    console.log('error');
      next(err);
      return;
    }
    if (!rows) {
+     console.log('wrong');
      res.status(400);
      res.send('Invalid username or password');
      return
    }
    rows.forEach((row) => {
-     if (row.username == req.body.username && row.password == req.body.password) {
+     if (row.username === req.body.username && row.password === req.body.password) {
          x = 1;
+         console.log('x1');
      }
      else {
          x = 2;
-         db.close();
+         console.log('x2');
      }
    })
-   if (x == 1) {
-     res.redirect('/');
-     console.log('eingeloggt');
-   }
-   else { res.redirect('/'); }
-   console.log('fehlgeschlagen');
-  })
+   if (x === 1) {
+    res.redirect('/index3');
+  }
+  else { res.redirect('/index2'); }
+})
 });
   
 

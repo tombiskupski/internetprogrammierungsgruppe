9dcdbacb28e120ca394de0d6aee6fde6eebfade5
Trung Son Nguyen <91ngtr1bif@hft-stuttgart.de>
2020-01-08 20:33:55 +0100

    new login base

--- index.js
+++ index.js
@@ -9,36 +9,34 @@ app.set('view engine', 'ejs');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-  app.post('/Login', (req, res, next) => {
-    let sql = `SELECT * FROM User WHERE username = "${req.body.username}" AND password = "${req.body.password}"`;
-    var x;
-   
-    db.all(sql, (err, rows) => {
-     if (err) {
-       next(err);
-       return;
-     }
-     if (!rows) {
-       res.status(400);
-       res.send('Invalid username or password');
-       return
-     }
-     rows.forEach((row) => {
-       if (row.username === req.body.username && row.password === req.body.password) {
-           x = 1;
-       }
-       else {
-           x = 2;
-           db.close();
-       }
-     })
-     if (x === 1) {
-       console.log('2');
-     }
-     else {console.log('1'); }
-    })
-  });
+app.get('/', function(req, res){
+  res.render('pages/index');
+});
 
+app.post('/Login', (req, res, next) => {
+  let sql = `SELECT * FROM User WHERE username = "${req.body.username}" AND password = "${req.body.password}"`;
+  var x;
+ 
+  db.all(sql, (err, rows) => {
+   if (err) {
+     next(err);
+     return;
+   }
+   if (!rows) {
+     res.status(400);
+     res.send('Invalid username or password');
+     return
+   }
+   rows.forEach((row) => {
+     if (row.username === req.body.username && row.password === req.body.password) {
+         console.log('1');
+     }
+     else {
+         console.log('2');
+     }
+   })
+  })
+})
 
 app.listen(port, function() {
     console.log(`Server listening on port ${port}…`)

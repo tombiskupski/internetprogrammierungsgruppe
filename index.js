c2b3c34462c7ee3f3ad74878bfbe8095e66ab570
Trung Son Nguyen <91ngtr1bif@hft-stuttgart.de>
2020-01-09 15:46:55 +0100

    login

--- index.js
+++ index.js
@@ -23,62 +23,53 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/index', function (req, res) {
+app.get('/login', function (req, res) {
   console.log('POST /add-entry');
   console.log(req.body);
-  db.run('INSERT INTO comment(aktuellernutzer, kommentar) VALUES (?, ?);',
+  db.run('INSERT INTO comment (aktuellernutzer, kommentar) VALUES (?, ?);',
     [req.body.loginname, req.body.comment],
     (err) => {
       if (err) {
         console.log(err);
-        res.render('pages/index');
+        res.render('/');  
       } else {
         res.redirect('/');
       }
     })
-	
 });
 
-var x;
 app.post('/index', function (req, res) {
 	
- let sql = `SELECT * FROM user WHERE username1 = "${req.body.loginname}" AND password1 = "${req.body.password}"`;
- var x;
-
- db.all(sql, (err, rows) => {
-  if (err) {
-    next(err);
-    return;
-  }
-  if (!rows) {
-    res.status(400);
-    res.send('Invalid username or password');
-    return
-  }
-  rows.forEach((row) => {
-    if (row.username1 === req.body.loginname && row.password1 === req.body.password) {
-        x = 1;
-    }
-    else {
-        x = 2;   
-    }
+  let sql = `SELECT * FROM user WHERE username1 = "${req.body.loginname}" AND password1 = "${req.body.password}"`;
+  var x;
+ 
+  db.all(sql, (err, rows) => {
+   if (err) {
+     next(err);
+     return;
+   }
+   if (!rows) {
+     res.status(400);
+     res.send('Invalid username or password');
+     return
+   }
+   rows.forEach((row) => {
+     if (row.username1 === req.body.loginname && row.password1 === req.body.password) {
+         x = 1;
+     }
+     else {
+         x = 2;   
+     }
+   })
+   if (x === 1) {
+     res.render('pages/login');
+     console.log('login');
+   }
+   else { res.redirect('/');
+   console.log('guest');
+ }
   })
-  if (x === 1) {
-<<<<<<< HEAD
-	  return textarea;
-    res.redirect('/index');
-  }
-  else { res.redirect('/index'); }
-=======
-    res.render('pages/login');
-    console.log('login');
-  }
-  else { res.redirect('/');
-  console.log('guest');
-}
->>>>>>> 1239de348ee8b8f1749d7bf87bfbd1a91d86e84d
- })
- });
+  });
   
 
 const server = app.listen(port, () => {

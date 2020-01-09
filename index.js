44d5e8d37560bbc49c15b801e2eb1a26ea846dcf
Henri Reber <91rehe1bif@hft-stuttgart.de>
2020-01-09 03:24:19 +0100

    login fix

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const app = express();
 const db = new sqlite3.Database('./datenbank/data.db');
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
-  res.render('pages/');
+app.get('/', function (req, res) {
+  console.log('GET1 /');
+  
+  db.all('SELECT * FROM user', (err, rows) => {
+	  
+    res.render('pages/index', {
+		
+      data: rows,
+     
+    });
+ 
+  })
 });
 
 app.post('/index', function (req, res) {
-  let sql = 'SELECT * FROM user WHERE username = "${req.body.username}" AND password = "${req.body.password}"';
-  var x;
-  console.log('db test');
-  db.all(sql, (err, rows) => {
-    console.log('db');
-   if (err) {
-    console.log('error');
-     next(err);
-     return;
-   }
-   if (!rows) {
-     console.log('wrong');
-     res.status(400);
-     res.send('Invalid username or password');
-     return
-   }
-   rows.forEach((row) => {
-     if (row.username === req.body.username && row.password === req.body.password) {
-         x = 1;
-         console.log('x1');
-     }
-     else {
-         x = 2;
-         console.log('x2');
-     }
-   })
-   if (x === 1) {
-    res.redirect('/index3');
+	
+ let sql = `SELECT * FROM user WHERE username1 = "${req.body.loginname}" AND password1 = "${req.body.password}"`;
+ var x;
+
+ db.all(sql, (err, rows) => {
+  if (err) {
+    next(err);
+    return;
   }
-  else { res.redirect('/index2'); }
-})
-});
+  if (!rows) {
+    res.status(400);
+    res.send('Invalid username or password');
+    return
+  }
+  rows.forEach((row) => {
+    if (row.username1 === req.body.loginname && row.password1 === req.body.password) {
+        x = 1;
+    }
+    else {
+        x = 2;
+        
+    }
+  })
+  if (x === 1) {
+    res.redirect('/index312313');
+  }
+  else { res.redirect('/index424234'); }
+ })
+ });
   
 
-app.listen(port, function() {
-    console.log(`Server listening on port ${port}…`)
-   });
+const server = app.listen(port, () => {
+  console.log(`Server listening on port ${port}…`)
+});
+
+module.exports = server;
